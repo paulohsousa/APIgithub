10bcad1d52ccf4b17db4f9bfa1d8ab61d466449f
Paulo Henrique de Sousa <paulo_henrique-16@live.com>
2024-07-04 16:14:19 -0300

    Criando tela inicial,criando tela de repositorios

---
+++ src/pages/RepositoriesPage/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+
+function RepositoriesPage() {
+  return <h1>Repositorios</h1>
+}
+
+
+
+
+export default RepositoriesPage
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes  } from "react-router-dom";
 
-import MainPage from "./pages/Main";
+import MainPage from "./pages/MainPage";
+import RepositoriesPage from "./pages/RepositoriesPage";
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/repositories"  element={<RepositoriesPage />}/>
+
       <Route path="/" element={<MainPage />} />
+      
+
+
     </Routes>
+
+
   );
 }
